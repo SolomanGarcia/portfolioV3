fe791cb8359ff5aec2a13dd9634cbe442b861fbc
Soloman Garcia <soloman.garcia@gmail.com>
2022-08-13 11:37:04 -0600

    refactor, create stickyElement function

--- script.js
+++ script.js
@@ -49,22 +49,15 @@ const animateCircles = (e, x, y) => {
 };
 // End of Animated Circles
 
+// Sticky Element
+
 let hoveredElPosition = [];
 
-document.body.addEventListener('mousemove', (e) => {
-  let x = e.clientX;
-  let y = e.clientY;
-
-  mouseCirlceFn(x, y);
-  animateCircles(e, x, y);
-
-  // Sticky Element
-  const hoveredEl = document.elementFromPoint(x, y);
-
+const stickyElement = (x, y, hoveredEl) => {
   if (hoveredEl.classList.contains('sticky')) {
-    if (hoveredElPosition.length < 1) {
-      hoveredElPosition = [hoveredEl.offsetTop, hoveredEl.offsetLeft];
-    }
+    hoveredElPosition.length < 1 &&
+      (hoveredElPosition = [hoveredEl.offsetTop, hoveredEl.offsetLeft]);
+
 
     hoveredEl.style.cssText = `top: ${y}px; left: ${x}px`;
 
@@ -79,6 +72,18 @@ document.body.addEventListener('mousemove', (e) => {
     };
   }
   // End of Sticky Element
+};
+
+document.body.addEventListener('mousemove', (e) => {
+  let x = e.clientX;
+  let y = e.clientY;
+
+  mouseCirlceFn(x, y);
+  animateCircles(e, x, y);
+
+  const hoveredEl = document.elementFromPoint(x, y);
+
+  stickyElement(x, y, hoveredEl);
 });
 
 document.body.addEventListener("mouseleave", () => {
