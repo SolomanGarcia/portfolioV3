f695023a4d9fcce5c28a8fc05b2655aa0dbb3afc
Soloman Garcia <soloman.garcia@gmail.com>
2022-08-24 08:56:02 -0600

    clean up code

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const mouseDot = document.querySelector(".mouse-dot");
 let mouseCircleBool = true;
 
 const mouseCirlceFn = (x, y) => {
-  mouseCircleBool && (mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`);
+  mouseCircleBool &&
+    (mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`);
 
   mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
 };
@@ -13,15 +14,14 @@ const mouseCirlceFn = (x, y) => {
 //End of Mouse Cirlce
 
 // Animated Circles
-const circles = document.querySelectorAll('.circle');
-const mainImg = document.querySelector('.main-circle img');
+const circles = document.querySelectorAll(".circle");
+const mainImg = document.querySelector(".main-circle img");
 
 let mX = 0;
 let mY = 0;
 const z = 100;
 
 const animateCircles = (e, x, y) => {
-
   if (x < mX) {
     circles.forEach((circle) => {
       circle.style.left = `${z}px`;
@@ -48,7 +48,6 @@ const animateCircles = (e, x, y) => {
 
   mX = e.clientX;
   mY = e.clientY;
-
 };
 // End of Animated Circles
 
@@ -57,14 +56,18 @@ let hoveredElPosition = [];
 // Sticky Element
 
 const stickyElement = (x, y, hoveredEl) => {
-  if (hoveredEl.classList.contains('sticky')) {
+  if (hoveredEl.classList.contains("sticky")) {
     hoveredElPosition.length < 1 &&
       (hoveredElPosition = [hoveredEl.offsetTop, hoveredEl.offsetLeft]);
 
-
     hoveredEl.style.cssText = `top: ${y}px; left: ${x}px`;
 
-    if (hoveredEl.offsetTop <= hoveredElPosition[0] - 100 || hoveredEl.offsetTop >= hoveredElPosition[0] + 100 || hoveredEl.offsetLeft <= hoveredElPosition[1] - 100 || hoveredEl.offsetLeft >= hoveredElPosition[1] + 100) {
+    if (
+      hoveredEl.offsetTop <= hoveredElPosition[0] - 100 ||
+      hoveredEl.offsetTop >= hoveredElPosition[0] + 100 ||
+      hoveredEl.offsetLeft <= hoveredElPosition[1] - 100 ||
+      hoveredEl.offsetLeft >= hoveredElPosition[1] + 100
+    ) {
       hoveredEl.style.cssText = "";
       hoveredElPosition = [];
     }
@@ -79,7 +82,7 @@ const stickyElement = (x, y, hoveredEl) => {
 
 // Mouse Circle Transform
 const mouseCircleTransform = (hoveredEl) => {
-  if (hoveredEl.classList.contains('pointer-enter')) {
+  if (hoveredEl.classList.contains("pointer-enter")) {
     hoveredEl.onmousemove = () => {
       mouseCircleBool = false;
       mouseCircle.style.cssText = `
@@ -101,14 +104,16 @@ const mouseCircleTransform = (hoveredEl) => {
 
     document.onscroll = () => {
       if (!mouseCircleBool) {
-        mouseCircle.style.cssText = `${hoveredEl.getBoundingClientRect().top}px`;
+        mouseCircle.style.cssText = `${
+          hoveredEl.getBoundingClientRect().top
+        }px`;
       }
     };
   }
 };
 // End of Mouse Circle Transform
 
-document.body.addEventListener('mousemove', (e) => {
+document.body.addEventListener("mousemove", (e) => {
   let x = e.clientX;
   let y = e.clientY;
 
@@ -123,28 +128,28 @@ document.body.addEventListener('mousemove', (e) => {
 });
 
 document.body.addEventListener("mouseleave", () => {
-  mouseCircle.style.opacity = '0';
-  mouseDot.style.opacity = '0';
+  mouseCircle.style.opacity = "0";
+  mouseDot.style.opacity = "0";
 });
 
 // Main Button
-const mainBtns = document.querySelectorAll('.main-btn');
+const mainBtns = document.querySelectorAll(".main-btn");
 
-mainBtns.forEach(btn => {
+mainBtns.forEach((btn) => {
   let ripple;
 
-  btn.addEventListener('mouseenter', e => {
+  btn.addEventListener("mouseenter", (e) => {
     const left = e.clientX - e.target.getBoundingClientRect().left;
     const top = e.clientY - e.target.getBoundingClientRect().top;
 
-    ripple = document.createElement('div');
-    ripple.classList.add('ripple');
+    ripple = document.createElement("div");
+    ripple.classList.add("ripple");
     ripple.style.left = `${left}px`;
     ripple.style.top = `${top}px`;
     btn.prepend(ripple);
   });
 
-  btn.addEventListener('mouseleave', () => {
+  btn.addEventListener("mouseleave", () => {
     btn.removeChild(ripple);
   });
 });
@@ -152,11 +157,11 @@ mainBtns.forEach(btn => {
 // End of Main Button
 
 // Progress Bar
-const sections = document.querySelectorAll('section');
-const progressBar = document.querySelector('.progress-bar');
-const halfCircles = document.querySelectorAll('.half-circle');
-const halfCircleTop = document.querySelector('.half-circle-top');
-const progressBarCircle = document.querySelector('.progress-bar-circle');
+const sections = document.querySelectorAll("section");
+const progressBar = document.querySelector(".progress-bar");
+const halfCircles = document.querySelectorAll(".half-circle");
+const halfCircleTop = document.querySelector(".half-circle-top");
+const progressBarCircle = document.querySelector(".progress-bar-circle");
 
 let scrolledPortion = 0;
 let scrollBool = false;
@@ -175,16 +180,17 @@ const progressBarFn = (bigImgWrapper) => {
     scrolledPortion = imageWrapper.scrollTop;
   }
 
-  const scrolledPortionDegree = (scrolledPortion / (pageHeight - pageViewportHeight)) * 360;
+  const scrolledPortionDegree =
+    (scrolledPortion / (pageHeight - pageViewportHeight)) * 360;
 
   halfCircles.forEach((el) => {
     el.style.transform = `rotate(${scrolledPortionDegree}deg)`;
 
     if (scrolledPortionDegree >= 180) {
       halfCircles[0].style.transform = "rotate(180deg)";
-      halfCircleTop.style.opacity = '0';
+      halfCircleTop.style.opacity = "0";
     } else {
-      halfCircleTop.style.opacity = '1';
+      halfCircleTop.style.opacity = "1";
     }
   });
 
@@ -204,8 +210,8 @@ progressBar.addEventListener("click", (e) => {
   e.preventDefault();
 
   if (!imageWrapper) {
-    const sectionPositions = Array.from(sections).map((section) =>
-      scrolledPortion + section.getBoundingClientRect().top
+    const sectionPositions = Array.from(sections).map(
+      (section) => scrolledPortion + section.getBoundingClientRect().top
     );
 
     const position = sectionPositions.find((sectionPosition) => {
@@ -214,7 +220,9 @@ progressBar.addEventListener("click", (e) => {
 
     scrollBool ? window.scrollTo(0, 0) : window.scrollTo(0, position);
   } else {
-    scrollBool ? imageWrapper.scrollTo(0, 0) : imageWrapper.scrollTo(0, imageWrapper.scrollHeight);
+    scrollBool
+      ? imageWrapper.scrollTo(0, 0)
+      : imageWrapper.scrollTo(0, imageWrapper.scrollHeight);
   }
 });
 // End of Progress Bar Click
@@ -224,38 +232,39 @@ progressBarFn();
 // End of Progress Bar
 
 // Navigation
-const menuIcon = document.querySelector('.menu-icon');
-const navbar = document.querySelector('.navbar');
+const menuIcon = document.querySelector(".menu-icon");
+const navbar = document.querySelector(".navbar");
 
 const scrollFn = () => {
-  menuIcon.classList.add('show-menu-icon');
-  navbar.classList.add('hide-navbar');
+  menuIcon.classList.add("show-menu-icon");
+  navbar.classList.add("hide-navbar");
 
   if (window.scrollY === 0) {
-    menuIcon.classList.remove('show-menu-icon');
-    navbar.classList.remove('hide-navbar');
+    menuIcon.classList.remove("show-menu-icon");
+    navbar.classList.remove("hide-navbar");
   }
 
   progressBarFn();
 };
-document.addEventListener('scroll', scrollFn);
+document.addEventListener("scroll", scrollFn);
 
 menuIcon.addEventListener("click", () => {
-  menuIcon.classList.remove('show-menu-icon');
-  navbar.classList.remove('hide-navbar');
+  menuIcon.classList.remove("show-menu-icon");
+  navbar.classList.remove("hide-navbar");
 });
 // End of Navigation
 
 // About Me Text
-const aboutMeText = document.querySelector('.about-me-text');
-const aboutMeTextContent = 'I’m a front-end developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices and writing clean JavaScript and React.';
+const aboutMeText = document.querySelector(".about-me-text");
+const aboutMeTextContent =
+  "I’m a front-end developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices and writing clean JavaScript and React.";
 
 Array.from(aboutMeTextContent).forEach((char) => {
-  const span = document.createElement('span');
+  const span = document.createElement("span");
   span.textContent = char;
   aboutMeText.appendChild(span);
 
-  span.addEventListener('mouseenter', (e) => {
+  span.addEventListener("mouseenter", (e) => {
     e.target.style.animation = "aboutMeTextAnim 10s infinite";
   });
 });
@@ -263,30 +272,32 @@ Array.from(aboutMeTextContent).forEach((char) => {
 // End of About Me Text
 
 // Projects
-const container = document.querySelector('.container');
-const projects = document.querySelectorAll('.project');
-const projectHideBtn = document.querySelector('.project-hide-btn');
+const container = document.querySelector(".container");
+const projects = document.querySelectorAll(".project");
+const projectHideBtn = document.querySelector(".project-hide-btn");
 
 projects.forEach((project, i) => {
-  project.addEventListener('mouseenter', () => {
-    project.firstElementChild.style.top = `-${project.firstElementChild.offsetHeight - project.offsetHeight + 20}px`;
+  project.addEventListener("mouseenter", () => {
+    project.firstElementChild.style.top = `-${
+      project.firstElementChild.offsetHeight - project.offsetHeight + 20
+    }px`;
   });
 
-  project.addEventListener('mouseleave', () => {
+  project.addEventListener("mouseleave", () => {
     project.firstElementChild.style.top = "2rem";
   });
 
   // Big Project Image
-  project.addEventListener('click', () => {
-    const bigImgWrapper = document.createElement('div');
-    bigImgWrapper.className = 'project-img-wrapper';
+  project.addEventListener("click", () => {
+    const bigImgWrapper = document.createElement("div");
+    bigImgWrapper.className = "project-img-wrapper";
     container.appendChild(bigImgWrapper);
 
-    const bigImg = document.createElement('img');
-    bigImg.className = 'project-img';
-    const imgPath = project.firstElementChild.getAttribute('src').split('.')[1];
+    const bigImg = document.createElement("img");
+    bigImg.className = "project-img";
+    const imgPath = project.firstElementChild.getAttribute("src").split(".")[1];
     console.log(imgPath);
-    bigImg.setAttribute('src', `${imgPath}-big.jpg`);
+    bigImg.setAttribute("src", `${imgPath}-big.jpg`);
     bigImgWrapper.appendChild(bigImg);
     document.body.style.overflowY = "hidden";
 
@@ -300,12 +311,12 @@ projects.forEach((project, i) => {
       progressBarFn(bigImgWrapper);
     };
 
-    projectHideBtn.classList.add('change');
+    projectHideBtn.classList.add("change");
 
     projectHideBtn.onclick = () => {
-      projectHideBtn.classList.remove('change');
+      projectHideBtn.classList.remove("change");
       bigImgWrapper.remove();
-      document.body.style.overflowY = 'scroll';
+      document.body.style.overflowY = "scroll";
 
       document.addEventListener("scroll", scrollFn);
 
@@ -318,15 +329,15 @@ projects.forEach((project, i) => {
 });
 
 // Projects Button
-const section3 = document.querySelector('.section-3');
-const projectsBtn = document.querySelector('.projects-btn');
-const projectsBtnText = document.querySelector('.projects-btn span');
+const section3 = document.querySelector(".section-3");
+const projectsBtn = document.querySelector(".projects-btn");
+const projectsBtnText = document.querySelector(".projects-btn span");
 let showHideBool = true;
 
 const showProjects = (project, i) => {
   setTimeout(() => {
     project.style.display = "flex";
-    section3.scrollIntoView({ block: 'end' });
+    section3.scrollIntoView({ block: "end" });
   }, 600);
 
   setTimeout(() => {
@@ -337,7 +348,7 @@ const showProjects = (project, i) => {
 const hideProjects = (project, i) => {
   setTimeout(() => {
     project.style.display = "none";
-    section3.scrollIntoView({ block: 'end' });
+    section3.scrollIntoView({ block: "end" });
   }, 1200);
 
   setTimeout(() => {
@@ -345,15 +356,18 @@ const hideProjects = (project, i) => {
   }, i * 100);
 };
 
-projectsBtn.addEventListener('click', (e) => {
+projectsBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
   projectsBtn.firstElementChild.nextElementSibling.classList.toggle("change");
 
-  showHideBool ? (projectsBtnText.textContent = 'Show Less') : (projectsBtnText.textContent = 'Show More');
+  showHideBool
+    ? (projectsBtnText.textContent = "Show Less")
+    : (projectsBtnText.textContent = "Show More");
 
   projects.forEach((project, i) => {
-    i >= 3 && (showHideBool ? showProjects(project, i) : hideProjects(project, i));
+    i >= 3 &&
+      (showHideBool ? showProjects(project, i) : hideProjects(project, i));
   });
   showHideBool = !showHideBool;
 });
@@ -361,14 +375,16 @@ projectsBtn.addEventListener('click', (e) => {
 // End of Projects
 
 // Section 4
-document.querySelectorAll('.skill-btn').forEach((skill) => {
-  skill.addEventListener('click', (e) => {
+document.querySelectorAll(".skill-btn").forEach((skill) => {
+  skill.addEventListener("click", (e) => {
     e.preventDefault();
 
     const skillText = skill.nextElementSibling;
-    skillText.classList.toggle('change');
+    skillText.classList.toggle("change");
 
-    const rightPosition = skillText.classList.contains('change') ? `calc(100% - ${getComputedStyle(skill.firstElementChild).width})` : 0;
+    const rightPosition = skillText.classList.contains("change")
+      ? `calc(100% - ${getComputedStyle(skill.firstElementChild).width})`
+      : 0;
 
     skill.firstElementChild.style.right = rightPosition;
   });
@@ -377,18 +393,18 @@ document.querySelectorAll('.skill-btn').forEach((skill) => {
 
 // Section 5
 // Form
-const formHeading = document.querySelector('.form-heading');
-const formInputs = document.querySelectorAll('.contact-form-input');
+const formHeading = document.querySelector(".form-heading");
+const formInputs = document.querySelectorAll(".contact-form-input");
 
-formInputs.forEach(input => {
-  input.addEventListener('focus', () => {
+formInputs.forEach((input) => {
+  input.addEventListener("focus", () => {
     formHeading.style.opacity = "0";
     setTimeout(() => {
       formHeading.textContent = `Your ${input.placeholder}`;
       formHeading.style.opacity = "1";
     }, 300);
   });
-  input.addEventListener('blur', () => {
+  input.addEventListener("blur", () => {
     formHeading.style.opacity = "0";
     setTimeout(() => {
       formHeading.textContent = "Let's Talk";
@@ -399,7 +415,7 @@ formInputs.forEach(input => {
 // End of Form
 
 // Slideshow
-const slideshow = document.querySelector('.slideshow');
+const slideshow = document.querySelector(".slideshow");
 
 setInterval(() => {
   const firstIcon = slideshow.firstElementChild;
@@ -408,9 +424,9 @@ setInterval(() => {
 
   const thirdIcon = slideshow.children[3];
 
-  thirdIcon.classList.add('light');
+  thirdIcon.classList.add("light");
 
-  thirdIcon.previousElementSibling.classList.remove('light');
+  thirdIcon.previousElementSibling.classList.remove("light");
 
   setTimeout(() => {
     slideshow.removeChild(firstIcon);
@@ -425,12 +441,12 @@ setInterval(() => {
 // End of Slideshow
 
 // Form Validation
-const form = document.querySelector('.contact-form');
-const username = document.getElementById('name');
-const email = document.getElementById('email');
-const subject = document.getElementById('subject');
-const message = document.getElementById('message');
-const messages = document.querySelectorAll('.message');
+const form = document.querySelector(".contact-form");
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+const messages = document.querySelectorAll(".message");
 
 const error = (input, message) => {
   input.nextElementSibling.classList.add("error");
@@ -442,7 +458,7 @@ const success = (input) => {
 };
 
 const checkRequiredFields = (inputArr) => {
-  inputArr.forEach(input => {
+  inputArr.forEach((input) => {
     if (input.value.trim() === "") {
       error(input, `${input.id} is required`);
     }
@@ -468,7 +484,7 @@ const checkEmail = (input) => {
   }
 };
 
-form.addEventListener("submit", e => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   checkLength(username, 2);
